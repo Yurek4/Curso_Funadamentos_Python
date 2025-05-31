407e64903711622125b93e4ebb12a9486213c939
Yuri Dev <147882154+Yurek4@users.noreply.github.com>
2025-05-31 15:50:22 -0300

    Aula_01
    Entrada de dados:
    
    O programa pede ao usuário para inserir seu nome, idade e altura, armazenando esses valores em variáveis nome, idade e altura.
    
    A função input() captura as informações, sendo que idade é convertida para um número inteiro com int() e altura para um número com casas decimais usando float().
    
    Processamento de informações:
    
    O código calcula o ano de nascimento do usuário subtraindo a idade de 2025. Vale observar que isso não leva em consideração o mês e dia exatos do nascimento, podendo resultar em uma estimativa imprecisa.
    
    Exibição dos dados:
    
    Através de comandos print(), o programa exibe uma saudação ao usuário, informa o ano estimado de nascimento e exibe a altura com 2 casas decimais.
    
    Função imc():
    
    A função imc() é criada para calcular o Índice de Massa Corporal (IMC) do usuário. A função pede o peso, faz o cálculo do IMC usando a fórmula peso / (altura ** 2) e exibe o valor.
    
    O IMC calculado é retornado para uso posterior.
    
    Chamada da função imc():
    
    A função imc() é chamada no final para calcular e exibir o IMC do usuário.

---
+++ Aula_1.py
@@ -0,0 +1,24 @@
+#Primeiro programa
+#Autor:Yuri 
+#Rotina de entrada
+
+nome = input("Qual é o seu nome? ") #nome é uma variavel string
+idade = int(input("Quantos anos vc tem ? ")) #idade é uma variavel tipo inteira
+altura = float(input("Qual é a sua altura em metros? ")) #altura é uma variavel tipo float
+
+
+#Processando informações
+ano_nascimento = 2025 - idade #calcula o ano de nascimento
+
+#Imprimir os dados
+print(f"\n Olá, {nome}") #Exibir uma saudação!
+print(f"Você nasceu em {ano_nascimento}")#exibir o ano de nascimento
+print(f"Sua altura é {altura:.2f}")#exibir altura com 2 casas decimais
+
+def imc(): #Criando a função imc
+    peso = float(input("Qual é o seu peso em kg?")) # Pedindo o peso do usuário
+    imc_calc = peso / (altura ** 2 ) #Calcular o imc
+    print(f"Seu imc é {imc_calc}")#exibir o imc com 2 casas decimais 
+    return imc_calc #retorna o valor do imc
+
+imc() #chama a função imc
